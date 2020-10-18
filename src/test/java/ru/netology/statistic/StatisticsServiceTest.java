1ecf741029ebb64a93a0ee6cfb0412cc0f023932
vovaanik <vovaanik@yandex.ru>
2020-10-18 16:15:21 -0500

    Second commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,10 +10,10 @@ class StatisticsServiceTest {
   void findMax() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long[] expectedArray = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
 
-    long actual = service.findMax(incomesInBillions);
+    long actual = service.findMax(expectedArray);
 
     assertEquals(expected, actual);
   }
