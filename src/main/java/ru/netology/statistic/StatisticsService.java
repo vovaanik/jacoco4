91aa027072fbac03ff83b93168bae2d193ccc2f4
vovaanik <vovaanik@yandex.ru>
2020-10-21 15:19:08 -0500

    New tests

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,12 +1,22 @@
 package ru.netology.statistic;
 
-public class StatisticsService  {
 
-    public long findMax (long[] array) {
-        long current_max = array[0];
-        for (long income : array) {
-            current_max = income;
+public class StatisticsService {
+    public long CurrentMax;
+    public long Income;
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes) {
+            if (current_max < income)
+                current_max = income;
         }
-        return current_max;
+            return current_max;
     }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -4,17 +4,45 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
 
-    long[] expectedArray = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+  class StatisticsServiceTest {
+      StatisticsService service = new StatisticsService();
 
-    long actual = service.findMax(expectedArray);
 
-    assertEquals(expected, actual);
+      @Test
+      void findMax() {
+          StatisticsService service = new StatisticsService();
+
+          long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+          long expected = 12;
+          long actual = service.findMax(incomesInBillions);
+
+          assertEquals(expected, actual);
+      }
+
+      @Test
+      void shouldGetCurrentMax() {
+          long expected= 0;
+          long actual = service.CurrentMax;
+          assertEquals(expected, actual);
+      }
+      @Test
+      void shouldGetIncome() {
+          long expected = 0;
+          long actual = service.Income;
+          assertEquals(expected, actual);
+      }
   }
-}
+
+  /*
+  Calculate index of max income
+ public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        return current_max;
+*/
